4043228f118e5517c8cacda7dcf1038f36fbe2f0
phanthh <phanthh25701@gmail.com>
2022-06-08 10:36:09 +0300

    Fix themer

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -10,42 +10,44 @@ const Header: React.FC = () => {
     <div className="header">
       <h1 className="header-title">YABA</h1>
       <h4 className="header-subtitle">Yet Another Blog App</h4>
-      <ul className="navbar">
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/">Blog</Link>
-        </li>
-        {isLogin ? (
-          <>
-            <li>
-              <Link to="/profile">{`@${username}`}</Link>
-            </li>
-            <li>
-              <button
-                className="logout-button"
-                onClick={() => {
-                  if (window.confirm("Are you sure you want to logout ?")) {
-                    logout();
-                  }
-                }}
-              >
-                Log out
-              </button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link to="/login">Log in</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign up</Link>
-            </li>
-          </>
-        )}
-      </ul>
+      <nav>
+        <ol className="navbar">
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/">Blog</Link>
+          </li>
+          {isLogin ? (
+            <>
+              <li>
+                <Link to="/profile">{`@${username}`}</Link>
+              </li>
+              <li>
+                <button
+                  className="logout-button"
+                  onClick={() => {
+                    if (window.confirm("Are you sure you want to logout ?")) {
+                      logout();
+                    }
+                  }}
+                >
+                  Log out
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Log in</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign up</Link>
+              </li>
+            </>
+          )}
+        </ol>
+      </nav>
       <select
         className="theme-selector"
         value={theme}
